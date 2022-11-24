9bc0e11e3a7482df2084cef960153980f23268a6
Ethan Chi <ethanachi@gmail.com>
2022-11-24 14:32:02 -0800

    add missing files

---
+++ chirpy/core/response_generator/supernode.py
@@ -0,0 +1,162 @@
+import os
+import yaml
+
+from chirpy.core.callables import NamedCallable
+from chirpy.core.state_manager import StateManager
+from chirpy.core.regex import response_lists
+from chirpy.core.response_generator.response_type import *
+from chirpy.core.response_generator.neural_helpers import is_two_part, NEURAL_DECODE_CONFIG, get_random_fallback_neural_response
+from chirpy.core.response_generator.state import NO_UPDATE, BaseState, BaseConditionalState
+from chirpy.core.response_generator.neural_helpers import get_neural_fallback_handoff, neural_response_filtering
+from chirpy.core.response_generator.treelet import Treelet
+from chirpy.core.response_generator_datatypes import ResponseGeneratorResult, PromptResult, emptyResult, \
+	emptyResult_with_conditional_state, emptyPrompt, UpdateEntity, AnswerType
+from chirpy.core.response_generator.helpers import *
+from chirpy.core.response_priority import ResponsePriority
+from chirpy.core.util import load_text_file
+from typing import Set, Optional, List, Dict
+import logging
+import os
+
+from importlib import import_module
+
+from concurrent import futures
+
+logger = logging.getLogger('chirpylogger')
+
+
+# 		for cases in self.nlg_yamls[supernode]['unconditional_prompt']:
+# 	requirements = cases['entry_conditions']
+# 	matches_entry_criteria = True
+# 	for key in requirements:
+# 		if flags[key] != requirements[key]:
+# 			matches_entry_criteria = False
+# 			break
+# 	if matches_entry_criteria:
+# 		return cases['case_name'], cases['prompt']
+# return None
+
+CONDITION_STYLE_TO_BEHAVIOR = {
+	'is_none': (lambda val: (val is None)),
+	'is_true': (lambda val: (val is True)),
+	'is_false': (lambda val: (val is False)),
+	'is_value': (lambda val, target: (val == target)),
+}
+
+BASE_PATH = os.path.join(os.path.dirname(__file__), '../../symbolic_rgs')
+
+def lookup_value(contexts):
+	if '.' in value_name:
+		assert len(value_name.split('.')) == 2, "Only one namespace allowed."
+		namespace_name, value_name = value_name.split('.')
+		value = contexts[namespace_value][value_name]
+	else:
+		assert False, f"Need a namespace for entry condition {value_name}."
+
+def evaluate_nlg_call(data, python_context, contexts):
+	if isinstance(data, str): # plain text
+		return data
+	
+	assert isinstance(data, dict) and len(data) == 1, f"Failure: data is {data}"
+	type = next(iter(data))
+	nlg_params = data[type]
+	if type == 'eval':
+		assert isinstance(nlg_params, str)
+		return effify(nlg_params, global_context=python_context)
+	elif type == 'val':
+		assert isinstance(nlg_params, str)
+		return lookup_value(nlg_params, contexts)
+	elif type == 'nlg_helper':
+		assert isinstance(nlg_params, dict)
+		function_name = nlg_params['name']
+		assert function_name in python_context
+		args = [rg] + data.get('args', [])   # Add RG as first argument
+		return python_context[function_name](*args)
+	elif type == 'inflect':
+		assert isinstance(nlg_params, dict)
+		inflect_token = nlg_params['inflect_token']
+		return lookup_value(nlg_params, contexts)
+	elif type == 'inflect_helper':
+		assert isinstance(nlg_params, dict)
+		inflect_function = nlg_params['type']
+		inflect_input = self.evaluate_nlg_call(nlg_params['str'], python_context, contexts)
+		return getattr(engine, inflect_function)(inflect_input)
+	else:
+		assert False, f"Generation type {type} not found!"
+		
+def evaluate_nlg_calls(datas, python_context, contexts):
+	output = []
+	if isinstance(datas, str):
+		datas = [datas]
+	for elem in datas:
+		output.append(evaluate_nlg_call(elem, python_context, contexts))
+	
+	return ' '.join(output)
+
+class Subnode:
+	def __init__(self, data):
+		logger.warning(f"Data is, {data}")
+		self.data = data
+		self.entry_conditions = data.get('entry_conditions', {})
+		self.response = data.get('response')
+		self.name = data['node_name']
+		
+	def is_valid(self, contexts):
+		for condition_style, var_name in self.entry_conditions.items():
+			assert condition_style in CONDITION_STYLE_TO_BEHAVIOR, f"Condition style {condition_style} is not recognized!"
+			validity_func = CONDITION_STYLE_TO_BEHAVIOR[condition_style]
+			evaluated_value = self.lookup_value(var_name, contexts)
+			if not validity_func(evaluated_value): return False
+		return True
+		
+	def get_response(self, python_context, contexts):
+		return evaluate_nlg_calls(self.response, python_context, contexts)
+		
+	def __str__(self):
+		return f'Subnode({self.name})'
+
+class Supernode:
+	def __init__(self, name):
+		self.yaml_path = os.path.join(BASE_PATH, name)
+		with open(os.path.join(self.yaml_path, 'supernode.yaml'), 'r') as f:
+			self.content = yaml.safe_load(f)
+			
+		self.requirements = self.load_requirements(self.content['supernode_requirements'])
+		self.locals = self.content['locals']
+		self.subnodes = self.load_subnodes(self.content['subnodes'])
+		
+		self.nlu = import_module(f'chirpy.symbolic_rgs.{name}.nlu')
+		_ = import_module(f'chirpy.symbolic_rgs.{name}.nlg_helpers')		
+	
+	def load_requirements(self, requirements):
+		self.requirements = requirements
+		
+	def is_eligible(self, state):
+		return any(self.all_matches(self.requirement) in self.requirements)
+
+	def get_global_subnodes(self):
+		return []
+		
+	def load_subnodes(self, subnode_data):
+		return [Subnode(data) for data in subnode_data]
+	
+	def get_optimal_subnode(self, contexts):
+		possible_subnodes = [subnode for subnode in self.subnodes + self.get_global_subnodes() if subnode.is_valid(contexts)]
+		assert len(possible_subnodes), "No subnode found!"
+		
+		# for now, just return the first possible subnode
+		return possible_subnodes[0]
+		
+		return None
+		
+	def evaluate_locals(self, contexts):
+		output = {}
+		for local_key, local_values in self.locals.items():
+			output[local_key] = self.evaluate_nlg_calls(local_values, contexts)
+		return output
+		
+	def get_flags(self, rg, state, utterance):
+		return self.nlu.nlu_processing(rg, state, utterance, set())
+		
+	def __str__(self):
+		return f"Supernode({self.yaml_path})"
---
+++ chirpy/core/response_generator/symbolic_response_generator.py
@@ -0,0 +1,177 @@
+from chirpy.core.callables import NamedCallable
+from chirpy.core.state_manager import StateManager
+from chirpy.core.regex import response_lists
+from chirpy.core.response_generator.response_type import *
+from chirpy.core.response_generator.neural_helpers import is_two_part, NEURAL_DECODE_CONFIG, get_random_fallback_neural_response
+from chirpy.core.response_generator.state import NO_UPDATE, BaseSymbolicState, BaseSymbolicConditionalState
+from chirpy.core.response_generator.neural_helpers import get_neural_fallback_handoff, neural_response_filtering
+from chirpy.core.response_generator.treelet import Treelet
+from chirpy.core.response_generator_datatypes import ResponseGeneratorResult, PromptResult, emptyResult, \
+    emptyResult_with_conditional_state, emptyPrompt, UpdateEntity, AnswerType
+from chirpy.core.response_generator.helpers import *
+from chirpy.core.response_generator.response_generator import ResponseGenerator
+from chirpy.core.response_generator.supernode import Supernode, Subnode
+from chirpy.core.response_priority import ResponsePriority
+from chirpy.core.util import load_text_file
+from typing import Set, Optional, List, Dict
+import logging
+import os
+
+from importlib import import_module
+
+from concurrent import futures
+
+logger = logging.getLogger('chirpylogger')
+
+import os
+STOPWORDS_FILEPATH = os.path.join(os.path.dirname(__file__), '../../data/long_stopwords.txt')
+STOPWORDS = load_text_file(STOPWORDS_FILEPATH)
+
+
+class SymbolicResponseGenerator(ResponseGenerator):
+    name='SYMBOLIC_RESPONSE'
+    def __init__(self,
+                 state_manager,
+                 supernodes=[
+                    'FOOD__intro',
+                    'GLOBALS'
+                 ],
+                 ):
+
+        super().__init__(state_manager,  
+            can_give_prompts=True,
+            state_constructor=BaseSymbolicState,
+            conditional_state_constructor=BaseSymbolicConditionalState,
+        )
+        
+        logger.warning(f"Starting load process with supernodes {supernodes}.")
+        self.paths_to_supernodes = self.load_supernodes_from_paths(supernodes)
+        logger.warning(f"Supernodes are: {', '.join(str(x) for x in self.paths_to_supernodes.keys())}")
+                
+    def load_supernodes_from_paths(self, supernode_paths):   
+        return {path: Supernode(path) for path in supernode_paths}
+        
+    def get_global_flags(self, state, utterance):
+        return {"GlobalFlag__" + k.name: v for k, v in global_response_type_dict(self, utterance).items()} 
+                
+    def get_response(self, state) -> ResponseGeneratorResult:
+        logger.warning("Begin response for SymbolicResponseGenerator.")
+        
+        # Legacy response types
+        
+        self.state = state
+        
+        state, utterance, response_types = self.get_state_utterance_response_types()
+        needs_prompt = False
+        
+        # figure out what supernode we're in
+        
+        supernode_path = state.cur_supernode or 'GLOBALS'
+        supernode = self.paths_to_supernodes[supernode_path]
+        logger.warning(f"Currently, we are in supernode {supernode}.")
+            
+        python_context = get_context_for_supernode(supernode)
+        python_context.update({
+            'rg': self,
+            'state': state
+        })
+        
+        # perform nlu
+        
+        flags = self.get_global_flags(state, utterance)
+        flags.update(supernode.get_flags(self, state, utterance))
+        
+        logging.warning(f"Flags are: {flags}")
+        
+        # Process locals        
+        locals = {}
+        contexts = {
+            'flags': flags,
+            'locals': locals,
+            'state': state,
+        }
+        locals = supernode.evaluate_locals(contexts)
+        logger.warning(f"Finished evaluating locals: {'; '.join((k + ': ' + v) for (k, v) in locals.items())}")
+
+        # select subnode
+        subnode = supernode.get_optimal_subnode(contexts=contexts)
+        response = subnode.get_response(python_context, contexts)
+        logger.warning(f'Received {response} from subnode {subnode}.')
+        
+        # update state
+        state.update(subnode.get_state_updates())
+        state.update(supernode.get_global_state_updates())
+        
+        
+        return ResponseGeneratorResult(text=response, 
+                                       priority=Priority.STRONG_CONTINUE, 
+                                       needs_prompt=False,
+                                       state=state,
+                                       cur_entity=cur_entity, 
+                                       answer_type=answer_type,
+                                       conditional_state=self.state_module.ConditionalState(**subnode_state_updates)
+                                      )
+        
+        # post-subnode state updates
+        # expose_vars = self.get_exposed_subnode_vars(supernode, subnode_name)
+        # exposed_context = {}
+        # if expose_vars is not None:
+        #     for key in expose_vars:
+        #         exposed_context[key] = eval(expose_vars[key], context)
+        
+        # select new supernode
+        
+        # return
+        
+
+    
+
+    def update_state_if_chosen(self, state, conditional_state):
+        """
+        This method updates the internal state of the response generator,
+        given that this RG is chosen as the next turn by the bot dialog manager. This state is accessible given
+        the global state of the bot in the variable
+
+        global_state['response_generator_states'][self.name]
+
+        If the attribute value is NO_UPDATE: no update is done for that attribute.
+        Otherwise, the attribute value is updated.
+        If conditional_state is None: make no update other than saving the response types
+        """
+        response_types = self.get_cache(f'{self.name}_response_types')
+        logger.info(f"Got cache for {self.name} response_types: {response_types}")
+        if response_types is not None:
+            state.response_types = construct_response_types_tuple(response_types)
+
+        if conditional_state is None: return state
+
+        if conditional_state:
+            for attr in dir(conditional_state):
+                if not callable(getattr(conditional_state, attr)) and not attr.startswith("__"):
+                    val = getattr(conditional_state, attr)
+                    if val != NO_UPDATE: setattr(state, attr, val)
+        state.num_turns_in_rg += 1
+        return state
+
+    def update_state_if_not_chosen(self, state, conditional_state):
+        """
+        By default, this sets the prev_treelet_str and next_treelet_str to '' and resets num_turns_in_rg to 0.
+        Response types are also saved.
+        No other attributes are updated.
+        All other attributes in ConditionalState are set to NO-UPDATE
+        """
+        response_types = self.get_cache(f'{self.name}_response_types')
+        if response_types is not None:
+            state.response_types = construct_response_types_tuple(response_types)
+
+        state.prev_treelet_str = ''
+        state.next_treelet_str = ''
+        state.num_turns_in_rg = 0
+
+        return state
+
+    def set_user_attribute(self, attr_name, value):
+        setattr(self.state_manager.user_attributes, attr_name, value)
+
+    def get_user_attribute(self, attr_name, default):
+        return getattr(self.state_manager.user_attributes, attr_name, default)
