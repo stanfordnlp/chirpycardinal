21545f1e0a465238a109110c40dd9846b08932d0
jrmkim50 <jrmkim50@gmail.com>
2022-12-18 18:14:21 -0500

    added background flag for personal issues intro supernode to track whether something is a personal issue or not. We track this info in the background because when we transition between supernodes, we only have its information and the global flag information available. Background tracking lets us track global information in a clean way

--- chirpy/symbolic_rgs/PERSONALISSUES__intro/nlu.py
+++ chirpy/symbolic_rgs/PERSONALISSUES__intro/nlu.py
@@ -1,4 +1,5 @@
 from chirpy.core.response_generator.nlu import nlu_processing
+from chirpy.response_generators.personal_issues import personal_issues_helpers
 
 @nlu_processing
 def get_flags(rg, state, utterance):
@@ -6,4 +7,5 @@ def get_flags(rg, state, utterance):
 
 @nlu_processing
 def get_background_flags(rg, utterance):
-    return
+    is_personal_issue = personal_issues_helpers.is_personal_issue(rg, utterance)
+    ADD_NLU_FLAG('PERSONALISSUEINTRO_personal_sharing_negative', is_personal_issue)
